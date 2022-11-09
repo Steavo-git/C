ed4f06e39ba2d91173fbf9526b7ee21ce3a404d5
Steavo-git <steavobabu638@gmail.com>
2022-11-09 21:43:59 +0530

    v1.1.2 - included do_while_loop

---
+++ Basics/Loops/Do_while_loop.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+    int i=0;
+
+    do 
+
+    {   //this block executes the statements even if condition is false
+
+        printf("Hello there!\n");
+        i++;
+
+    } while(i<5);   //here the condition is being checked
+
+}
+
+//Code by Steavo Babu
