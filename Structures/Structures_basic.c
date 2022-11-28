c5f34cf51c065ae729d083452f09976bf7f0087c
Steavo171 <steavobabu638@gmail.com>
2022-11-28 21:43:35 +0530

    v6.0 - added structures

---
+++ Structures/Structures_basic.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main()
+{
+    //struct structurename
+    struct book
+    {
+        //structure elemenets of different datatypes
+        char name[30];int pages; float price;
+    };
+    //structure variables
+    struct book b1,b2,b3;
+
+    printf("Enter names,no of pages and price of the 3 books\n");
+
+    scanf("%8s%6d   %8f",&b1.name,&b1.pages,&b1.price);   //accessing structure elements with structure variables using [.] operator
+
+    printf("The following has been entered\n");
+    
+    printf("%8s%6d   %f",b1.name,b1.pages,b1.price); 
+    
+}
+
+//Code by Steavo Babu
