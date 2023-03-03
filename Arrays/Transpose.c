3d8264a54d232af7bcac24a72f18db857fbee1ae
Steavo171 <steavobabu638@gmail.com>
2023-03-03 09:28:37 +0530

    added comments

--- Arrays/Transpose.c
+++ Arrays/Transpose.c
@@ -23,7 +23,8 @@ void transpose(int tda[][4])
 		}
 }
 
-void chk(int tda[][4])
+void chk(int tda[][4])		// function to check if the matric is symmetric or not
+							//symmetric matrix is a matrix in which the original matrix and the transposed matrix remains
 {
 	int r,c,fl=0;
 	for(r = 0;r<4;r++)
@@ -43,12 +44,11 @@ void chk(int tda[][4])
 void main()
 
 {
-	//int tda[4][4] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-	int tda[4][4] = { 1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1};
+	int tda[4][4] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}; 		//non symmetric matrix
+	//int tda[4][4] = { 1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1};			//symmetric matrix	
 	
 	printf("Initial_Array\n");
 	printtda(tda);
-
 	transpose(tda);
 	printf("\nAfter transposing\n");	
 	printtda(tda);
